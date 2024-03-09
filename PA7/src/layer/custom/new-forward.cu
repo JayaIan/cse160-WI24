f47a20371000c97492f6fe707dcd62f3c033521c
ijayacha <ijayacha@ucsd.edu>
2024-03-09 11:25:54 -0800

    PA7 loop

--- PA7/src/layer/custom/new-forward.cu
+++ PA7/src/layer/custom/new-forward.cu
@@ -8,7 +8,17 @@
 
 __global__ void conv_forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
+// An example use of these macros:
+// float a = y4d(0,0,0,0)
+// y4d(0,0,0,0) = a
 
+    const int H_out = H - K + 1;
+    const int W_out = W - K + 1;
+    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
+    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
+#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
+#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
     /*
     Modify this function to implement the forward pass described in Chapter 16.
     We have added an additional dimension to the tensors to support an entire mini-batch
@@ -16,17 +26,20 @@ __global__ void conv_forward_kernel(float *y, const float *x, const float *k, co
     We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
     */
 
-    const int H_out = H - K + 1;
-    const int W_out = W - K + 1;
-    (void)H_out; // silence declared but never referenced warning. remove this line when you start working
-    (void)W_out; // silence declared but never referenced warning. remove this line when you start working
+    int W_grid = W_out/TILE_WIDTH; 	// number of horizontal tiles per output map
 
-// An example use of these macros:
-// float a = y4d(0,0,0,0)
-// y4d(0,0,0,0) = a
-#define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
-#define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
-#define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
+    int b = blockIdx.z;
+    int m = blockIdx.x;
+    int h = (blockIdx.y / W_grid) * TILE_WIDTH + threadIdx.y;
+    int w = (blockIdx.y % W_grid) * TILE_WIDTH + threadIdx.x;    
+    float acc = 0.;
+    for (int c = 0;  c < C; c++) {		// sum over all input channels
+       for (int p = 0; p < K; p++)		// loop over KxK  filter
+          for (int q = 0; q < K; q++)  
+            //  acc += X[b, c, h + p, w + q] * W[m, c, p, q];
+             acc += x4d(b, c, h+p, w+q) * k4d(m, c, p, q);
+    }
+    y4d(b, m, h, w) = acc;
 
 
 #undef y4d
